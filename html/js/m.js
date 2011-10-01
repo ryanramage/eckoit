afeb9b0b1f81623ec1a8b9c21fee5b3f9ce735ff
Ryan Ramage <ryan.ramage@gmail.com>
2011-09-30 23:16:19 -0600

    uTag screen looking ok.
    Tested on Chrome, FF, Safari, iOS, Andriod 3.x. Seems to be a
    reasonable layout.

--- html/js/m.js
+++ html/js/m.js
@@ -5,33 +5,90 @@ app.controller = app.controller || {};
 
 
 
+app.controller.init = function(details) {
+    if (!details.tag_groups) return;
+    $.each(details.tag_groups, function(i, group) {
+        app.controller.addTagGroup(group);
+    });
 
-app.slideCount = 5;
-
-app.updateDisks = function () {
-    var zeroIndexSlideCount = 0;
-    var pos = window.mySwipe.getPos();
-    var after = zeroIndexSlideCount - pos;
-    var before  = zeroIndexSlideCount - after;
-
-    var append = function(node, count) {
-        for (var i=0; i < count; i++) {
-            node.append('<span>&#8226;</span>');
-        }
-        
-    }
-
+    app.controller.showTagGroup(details.tag_groups[0]);
 
 }
 
 
+app.controller.addTagGroup = function(group) {
+    var content = $('<li><a href="#"  class="tab  '+group.name+'" >'+ group.name+ '</a></li>');
+    $('.tag-groups').append(content);
+    content.find('a').click(function() {
+        app.controller.showTagGroup(group);
+        $('.tag-group-tags').trigger('create');
+        $(window).trigger('resize');
+    });
+}
 
+
+app.controller.showTagGroup = function(group) {
+    $('.tag-group-tags').empty();
+    $.each(group.tags, function(i, tag) {
+        
+        var name = tag.name;
+        $('.tag-group-tags').append('<input type="checkbox" name="'+name+'" id="'+name+'" class="custom" /><label for="'+name+'">'+ name +'</label>');
+
+
+    });
+    $('.' + group.name).addClass('ui-btn-active');
+}
+
+
+
+$('#utag').live('pagecreate',function(event){
+      var details = {
+        tag_groups: [
+            {
+                name: "Home",
+                tags : [
+                    {
+                        name: "Funny"
+                    },
+                    {
+                        name : "Helarity"
+                    }
+                ]
+            },
+            {
+                name : "Work",
+                tags : [
+                    {
+                        name : "Todo"
+                    },
+                    {
+                        name : "Delegate"
+                    },
+                    {
+                        name : "Earthy"
+                    }
+                ]
+            }
+        ]
+    };
+    app.controller.init(details);
+});
 
 
 $(function() {
-    $(window).live('swipeleft', function() {
-        console.log('left');
-    });
+
+    $('.tab').live('click', function() {
+        $('.tab').removeClass('ui-btn-active');
+        $(this).addClass('ui-btn-active');
+        return false;
+    })
+
+
+
+
+
+
+
 
 
 });
