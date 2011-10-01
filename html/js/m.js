ddd29ccec3baeceafdd6d148693292bb12994586
Ryan Ramage <ryan.ramage@gmail.com>
2011-09-30 21:07:41 -0600

    Mock up with JQueryMobile
    Don't like the primary, secondary…simplify

--- html/js/m.js
+++ html/js/m.js
@@ -5,30 +5,11 @@ app.controller = app.controller || {};
 
 
 
-eckoit = eckoit || {};
-
-eckoit.utag_panel = function(node, options) {
-    this.node = node;
-    this.options = options || {}
-}
-eckoit.utag_panel.prototype = {
-    init: function(details) {
-        this.details = details;
-
-    },
-    render : function() {
-        
-    }
-
-
-
-}
-
 
 app.slideCount = 5;
 
 app.updateDisks = function () {
-    var zeroIndexSlideCount = app.slideCount - 1;
+    var zeroIndexSlideCount = 0;
     var pos = window.mySwipe.getPos();
     var after = zeroIndexSlideCount - pos;
     var before  = zeroIndexSlideCount - after;
@@ -40,12 +21,6 @@ app.updateDisks = function () {
         
     }
 
-    $('.header .before').empty();
-    append($('.header .before'), before);
-
-    $('.header .after').empty();
-    append($('.header .after'), after);
-
 
 }
 
@@ -54,23 +29,9 @@ app.updateDisks = function () {
 
 
 $(function() {
-
-    $('#slider li div').height($(window).height() - 200);
-
-
-    window.mySwipe = new Swipe(document.getElementById('slider'), {
-        callback : function() {
-            app.updateDisks();
-        }
+    $(window).live('swipeleft', function() {
+        console.log('left');
     });
 
-    app.updateDisks();
-    
-    $('.add').click(function() {
-        $('#slider ul').append("<li><div>ADDED</div></li>");
-        window.mySwipe.setup();
-        app.slideCount++;
-        app.updateDisks();
-    })
 
 });
