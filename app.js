1a6e4c068f34fc0fb101e0b76ad50b987554ecd6
Ryan Ramage <ryan.ramage@gmail.com>
2012-07-18 11:54:04 -0600

    remove stuff moved to other apps

--- app.js
+++ app.js
@@ -248,24 +248,6 @@ var couchapp = require('couchapp')
 
 
 
-  ddoc.updates.bumpVotes = function(doc, req) {
-      if (!doc) {
-          return [null, "Need an existing doc"];
-      } else {
-          if (!doc.votes) doc.votes = 1;
-          else doc.votes++;
-          return [doc, doc.votes + ""];
-      }
-  }
-  ddoc.updates.bumpViews = function(doc, req) {
-      if (!doc) {
-          return [null, "Need an existing doc"];
-      } else {
-          if (!doc.views) doc.views = 1;
-          else doc.views++;
-          return [doc, doc.views + ""];
-      }
-  }
 
 
 
